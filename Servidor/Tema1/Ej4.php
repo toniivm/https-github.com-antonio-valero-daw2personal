331d22180d39dd1c20373731c507759643e0026f
toniivm <toniuve7@gmail.com>
2025-09-25 10:40:43 +0200

    servidor ej

---
+++ Servidor/Tema1/Ej4.php
@@ -0,0 +1,18 @@
+<?php 
+$hora=14;
+switch ($hora) {
+    case 9:
+        echo "Hora de desayunar";
+        break;
+    case 14:
+        echo "Hora de comer";
+        break;
+    case 21:
+        echo "Hora de cenar";
+        break;
+    default:
+        echo "Ahora no toca comer";
+        break;
+
+}
+?>
