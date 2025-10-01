060ae21b6a517e15bb3ca77256cf819037217d57
toniivm <toniuve7@gmail.com>
2025-10-01 12:24:03 +0200

    cliente deal

---
+++ Servidor/Tema1/PracticaCodigos/Ejercicio10.php
@@ -0,0 +1,19 @@
+
+<?php
+/* Interpretación de variables.
+¿Cómo se declara una variable numerica en php?;
+¿Cómo se realiza una operación con variables numericas?;
+¿Es posible mostrar en pantalla en una misma linea cadenas y numeros?
+¿Cómo incrementamos una variable?
+*/
+$a = 8;
+$b = 3;
+echo $a + $b, "<br>";
+echo $a - $b, "<br>";
+echo $a * $b, "<br>";
+echo $a / $b, "<br>";
+$a++ ;
+echo $a,"<br>";
+$b--;
+echo $b,"<br>";
+?>
