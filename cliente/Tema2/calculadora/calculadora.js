74c81d69517acaaf9815d88ba1cf31619000e299
toniivm <toniuve7@gmail.com>
2025-10-30 14:18:54 +0100

    calcu

---
+++ cliente/Tema2/calculadora/calculadora.js
@@ -0,0 +1,25 @@
+let operacion = '';
+
+function agregarCifra(numero) {
+ operacion += numero;
+ updateDisplay();
+}
+
+function updateDisplay() {
+ document.getElementById('display').innerText = operacion;
+}
+
+function clearDisplay() {
+ operacion = '';
+ document.getElementById('display').innerText = operacion;
+}
+function calcularResultado() {
+    console.log("Calculando resultado de la operación: " + operacion);
+    let resultado = eval(operacion);
+    operacion = resultado.toString();
+    updateDisplay();
+}   
+
+// Asignar eventos a los botones numéricos
+let botonIgual = document.getElementById('boton-igual');
+botonIgual.addEventListener('click', calcularResultado);
